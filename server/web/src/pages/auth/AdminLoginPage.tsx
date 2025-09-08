8ce747adf7c844312daa35d0a80e0c1e09b00e15
Eldo Macuácua <eldomacuacua@yahoo.com>
2025-09-08 10:54:47 +0530

    auth warnigs

--- server/web/src/pages/auth/AdminLoginPage.tsx
+++ server/web/src/pages/auth/AdminLoginPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Card, CardContent, CardHeader, CardTitle } from '../../components/ui/card';
 import { Input } from '../../components/ui/input';
 import { Button } from '../../components/ui/button';
--- server/web/src/pages/auth/LoginPage.tsx
+++ server/web/src/pages/auth/LoginPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/ui/button';
 import { Input } from '../../components/ui/input';
 import { Card, CardHeader, CardTitle, CardContent } from '../../components/ui/card';
